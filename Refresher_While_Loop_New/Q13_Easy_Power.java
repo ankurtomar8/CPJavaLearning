1a9dcc79c6370d947c450c2981fed9f569a5c1e8
AnkurDev <ankurtomar8@outlook.com>
2025-04-20 19:30:37 +0530

    [Topic] While Loop [Day] 4 [Problems] Easy/Medium

---
+++ Refresher_While_Loop_New/Q13_Easy_Power.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Q13_Easy_Power {
+    
+    public static void main(String[] args){
+     
+        Scanner sc = new Scanner(System.in);
+
+        int startNumber = 1;
+        int baseNumber = sc.nextInt();
+        int exponent = sc.nextInt();
+       
+        if(exponent == 0){
+            System.out.print("1");
+        }
+        //  2 x 2 x 2
+        // 4 x 4 x 4 x 4 
+        while(exponent > 0){
+            startNumber =  startNumber * baseNumber;
+            exponent--;
+        }
+        System.out.print(startNumber);
+        
+    }
+}
