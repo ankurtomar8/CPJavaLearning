38f0ff0ca9621ae40f0635f2190bf1a76af73124
AnkurDev <ankurtomar8@outlook.com>
2025-05-04 10:45:05 +0530

    [Topic]  2D Array [May] Day 12 Problems [ProblemName] Add_the_matrices [Level] Easy/Medium [Appreciation] Good Job

---
+++ Refresher_2D_Arrays.java/Q5_Add_the_matrices.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Q5_Add_the_matrices {
+
+    public static void addTwoMatrices(int[][] matrixOne, int[][] matrixTwo){
+
+        int n = matrixOne.length;
+        int m = matrixOne[0].length;
+        
+        int matrixAdditon[][] = new int[n][m];
+        System.out.println("Rows: "+n+" Cols: "+m);
+        for(int row = 0; row < n ; row++){
+            for(int col = 0; col < m; col++){
+             //  System.out.print(matrixOne[row][col]+" ");
+             matrixAdditon[row][col] = matrixOne[row][col] + matrixTwo[row][col];
+             System.out.print(matrixAdditon[row][col]+" ");
+            } 
+            System.out.println("");           
+        } 
+            
+    }
+
+
+    public static void main(String args[]){
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // row
+        int[][] matrixOne = new int[n][n];
+        //  2D array input 
+        for(int row = 0; row < n ; row++){
+            for(int col = 0; col < n; col++){
+                matrixOne[row][col] = sc.nextInt();
+            }            
+        } 
+    
+        int[][] matrixTwo = new int[n][n];
+        //  2D array input 
+        for(int row = 0; row < n ; row++){
+            for(int col = 0; col < n; col++){
+                matrixTwo[row][col] = sc.nextInt();
+            }            
+        } 
+
+        addTwoMatrices(matrixOne,matrixTwo);
+     
+    }
+
+}
