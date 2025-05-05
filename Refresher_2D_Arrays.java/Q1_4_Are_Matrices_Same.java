d152a6d5b5c487e443e1c216db369b7ec32e1dd0
AnkurDev <ankurtomar8@outlook.com>
2025-05-05 10:06:46 +0530

    [Topic]  2D Array [May] Day 12 Problems [ProblemName] Are_Matrices_Same[Level] Easy/Medium [Appreciation] Good Job

---
+++ Refresher_2D_Arrays.java/Q1_4_Are_Matrices_Same.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Q1_4_Are_Matrices_Same {
+    
+    public static void compareTwoMatrices(int[][] matrixOne, int[][] matrixTwo){
+
+        int n = matrixOne.length;
+        int m = matrixOne[0].length;
+        
+       // int matrixAdditon[][] = new int[n][m];
+        System.out.println("Rows: "+n+" Cols: "+m);
+        for(int row = 0; row < n ; row++){
+            for(int col = 0; col < m; col++){
+
+             if( matrixOne[row][col] != matrixTwo[row][col]){
+                System.out.print(" False");
+             }else{
+                System.out.print(" True");
+             }
+             
+            } 
+            System.out.println("");           
+        } 
+            
+    }
+
+
+    public static void main(String args[]){
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // row
+        int[][] matrixOne = new int[n][n];
+        //  2D array input 
+        for(int row = 0; row < n ; row++){
+            for(int col = 0; col < n; col++){
+                matrixOne[row][col] = sc.nextInt();
+            }            
+        } 
+    
+        int[][] matrixTwo = new int[n][n];
+        //  2D array input 
+        for(int row = 0; row < n ; row++){
+            for(int col = 0; col < n; col++){
+                matrixTwo[row][col] = sc.nextInt();
+            }            
+        } 
+
+        compareTwoMatrices(matrixOne,matrixTwo);
+     
+    }
+}
