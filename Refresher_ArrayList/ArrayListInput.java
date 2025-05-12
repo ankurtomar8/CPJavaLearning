a12df7a900666119c9662b1767666b03bf0e8862
AnkurDev <ankurtomar8@outlook.com>
2025-05-12 23:26:59 +0530

    [Topic] ArrayList [Problems] Q1. Divisible by 5 and 7[Level]Easy/Medium

---
+++ Refresher_ArrayList/ArrayListInput.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class ArrayListInput {
+    public static ArrayList<Integer> multiples(ArrayList<Integer> arr){
+
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        for (int i = 0 ; i < arr.size();i++){
+            int val = arr.get(i);
+
+            if(val % 5 == 0 || val % 7 == 0){
+                ans.add(val);
+            }
+
+        }
+        return ans;
+
+    }
+
+    public static void main(String args[]){
+        Scanner scn = new Scanner(System.in);
+
+         ArrayList<Integer> arr = new ArrayList<Integer>();
+         int n = scn.nextInt();
+
+         for (int i = 0; i < n; i++){
+            int tmp = scn.nextInt();
+            arr.add(tmp);
+         }
+         System.out.println(arr);
+          System.out.println(multiples(arr));
+
+    }
+    
+}
